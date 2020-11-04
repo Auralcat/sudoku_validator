aedf7af20f20d8044f24650081b1d559b574ea8e
Auralcat <miriamretka@gmail.com>
2020-11-04 20:30:57 -0300

    Create grid accessor functions
    We need a way to grab the desired sections of the grid to iterate over.

--- lib/grid.ex
+++ lib/grid.ex
@@ -6,28 +6,86 @@ defmodule SudokuValidator.Grid do
   defdelegate extract(raw_grid), to: SudokuValidator.GridExtractor, as: :extract
 
   @doc """
-  Fetches the row for a given sudoku grid structure.
+  Fetches the row with the given order number for a given sudoku grid structure.
   """
-  def fetch_row(grid, row_number) do
-    case row_number do
-      row_number when row_number in 0..2 ->
-        ["left", "center", "right"]
-        |> Enum.map(fn pos -> Enum.at(grid["upper"][pos], row_number) end)
-        |> Enum.reduce(fn cols, acc ->
-          Enum.concat(acc, cols)
-        end)
-      row_number when row_number in 3..5 ->
-        ["left", "center", "right"]
-        |> Enum.map(fn pos -> Enum.at(grid["center"][pos], row_number - 3) end)
-        |> Enum.reduce(fn cols, acc ->
-          Enum.concat(acc, cols)
-        end)
-      row_number when row_number in 6..8 ->
-        ["left", "center", "right"]
-        |> Enum.map(fn pos -> Enum.at(grid["lower"][pos], row_number - 6) end)
-        |> Enum.reduce(fn cols, acc ->
-          Enum.concat(acc, cols)
-        end)
+  def fetch_row(%{"upper" => upper_subgroups}, row_number) when row_number in 0..2 do
+    ["left", "center", "right"]
+    |> Enum.map(fn pos -> Enum.at(upper_subgroups[pos], row_number) end)
+    |> concat_fetched_items()
+  end
+
+  def fetch_row(%{"center" => center_subgroups}, row_number) when row_number in 3..5 do
+    ["left", "center", "right"]
+    |> Enum.map(fn pos -> Enum.at(center_subgroups[pos], row_number - 3) end)
+    |> concat_fetched_items()
+  end
+
+  def fetch_row(%{"lower" => lower_subgroups}, row_number) when row_number in 6..8 do
+    ["left", "center", "right"]
+    |> Enum.map(fn pos -> Enum.at(lower_subgroups[pos], row_number - 6) end)
+    |> concat_fetched_items()
+  end
+
+  @doc """
+  Fetches the column for a given sudoku grid and the input column number.
+  """
+  def fetch_column(grid, col_number) when col_number in 0..2 do
+    ["upper", "center", "lower"]
+    |> Enum.map(fn section ->
+      Enum.map(0..2, fn pos ->
+        Enum.at(grid[section]["left"], pos) |> Enum.at(col_number)
+      end)
+    end)
+    |> concat_fetched_items()
+  end
+
+  def fetch_column(grid, col_number) when col_number in 3..5 do
+    ["upper", "center", "lower"]
+    |> Enum.map(fn section ->
+      Enum.map(0..2, fn pos ->
+        Enum.at(grid[section]["center"], pos) |> Enum.at(col_number - 3)
+      end)
+    end)
+    |> concat_fetched_items()
+  end
+
+  def fetch_column(grid, col_number) when col_number in 6..8 do
+    ["upper", "center", "lower"]
+    |> Enum.map(fn section ->
+      Enum.map(0..2, fn pos ->
+        Enum.at(grid[section]["right"], pos) |> Enum.at(col_number - 6)
+      end)
+    end)
+    |> concat_fetched_items()
+  end
+
+  def fetch_subgroup(grid, subgroup_number) do
+    case subgroup_number do
+      0 ->
+        grid["upper"]["left"]
+      1 ->
+        grid["upper"]["center"]
+      2 ->
+        grid["upper"]["right"]
+      3 ->
+        grid["center"]["left"]
+      4 ->
+        grid["center"]["center"]
+      5 ->
+        grid["center"]["right"]
+      6 ->
+        grid["lower"]["left"]
+      7 ->
+        grid["lower"]["center"]
+      8 ->
+        grid["lower"]["right"]
     end
+    |> concat_fetched_items()
+  end
+
+  defp concat_fetched_items(items) do
+    Enum.reduce(items, [], fn single_item, acc ->
+      Enum.concat(acc, single_item)
+    end)
   end
 end
--- test/grid_test.exs
+++ test/grid_test.exs
@@ -5,8 +5,38 @@ defmodule SudokuValidator.GridTest do
 
   test "fetch_row/2 returns the given row for a sudoku grid struct" do
     assert Grid.fetch_row(sample_complete_grid(), 0) == [8, 5, 9, 6, 1, 2, 4, 3, 7]
+    assert Grid.fetch_row(sample_complete_grid(), 1) == [7, 2, 3, 8, 5, 4, 1, 6, 9]
+    assert Grid.fetch_row(sample_complete_grid(), 2) == [1, 6, 4, 3, 7, 9, 5, 2, 8]
+    assert Grid.fetch_row(sample_complete_grid(), 3) == [9, 8, 6, 1, 4, 7, 3, 5 ,2]
     assert Grid.fetch_row(sample_complete_grid(), 4) == [3, 7, 5, 2, 6, 8, 9, 1, 4]
+    assert Grid.fetch_row(sample_complete_grid(), 5) == [2, 4, 1, 5, 9, 3, 7, 8 ,6]
+    assert Grid.fetch_row(sample_complete_grid(), 6) == [4, 3, 2, 9, 8, 1, 6, 7, 5]
+    assert Grid.fetch_row(sample_complete_grid(), 7) == [6, 1, 7, 4, 2, 5, 8, 9 ,3]
     assert Grid.fetch_row(sample_complete_grid(), 8) == [5, 9, 8, 7, 3, 6, 2, 4, 1]
+  end
+
+  test "fetch_column/2 returns the given column for a sudoku grid struct" do
+    assert Grid.fetch_column(sample_complete_grid(), 0) == [8, 7, 1, 9, 3, 2, 4, 6, 5]
+    assert Grid.fetch_column(sample_complete_grid(), 1) == [5, 2, 6, 8, 7, 4, 3, 1, 9]
+    assert Grid.fetch_column(sample_complete_grid(), 2) == [9, 3, 4, 6, 5, 1, 2, 7, 8]
+    assert Grid.fetch_column(sample_complete_grid(), 3) == [6, 8, 3, 1, 2, 5, 9, 4, 7]
+    assert Grid.fetch_column(sample_complete_grid(), 4) == [1, 5, 7, 4, 6, 9, 8, 2, 3]
+    assert Grid.fetch_column(sample_complete_grid(), 5) == [2, 4, 9, 7, 8, 3, 1, 5, 6]
+    assert Grid.fetch_column(sample_complete_grid(), 6) == [4, 1, 5, 3, 9, 7, 6, 8, 2]
+    assert Grid.fetch_column(sample_complete_grid(), 7) == [3, 6, 2, 5, 1, 8, 7, 9, 4]
+    assert Grid.fetch_column(sample_complete_grid(), 8) == [7, 9, 8, 2, 4, 6, 5, 3, 1]
+  end
+
+  test "fetch_subgroup/2 returns the given subgroup for a sudoku grid struct" do
+    assert Grid.fetch_subgroup(sample_complete_grid(), 0) == [8, 5, 9, 7, 2, 3, 1, 6, 4]
+    assert Grid.fetch_subgroup(sample_complete_grid(), 1) == [6, 1, 2, 8, 5, 4, 3, 7, 9]
+    assert Grid.fetch_subgroup(sample_complete_grid(), 2) == [4, 3, 7, 1, 6, 9, 5, 2, 8]
+    assert Grid.fetch_subgroup(sample_complete_grid(), 3) == [9, 8, 6, 3, 7, 5, 2, 4, 1]
+    assert Grid.fetch_subgroup(sample_complete_grid(), 4) == [1, 4, 7, 2, 6, 8, 5, 9, 3]
+    assert Grid.fetch_subgroup(sample_complete_grid(), 5) == [3, 5, 2, 9, 1, 4, 7, 8, 6]
+    assert Grid.fetch_subgroup(sample_complete_grid(), 6) == [4, 3, 2, 6, 1, 7, 5, 9 ,8]
+    assert Grid.fetch_subgroup(sample_complete_grid(), 7) == [9, 8, 1, 4, 2, 5, 7, 3, 6]
+    assert Grid.fetch_subgroup(sample_complete_grid(), 8) == [6, 7, 5, 8, 9, 3, 2, 4, 1]
   end
 
   defp sample_complete_grid do
