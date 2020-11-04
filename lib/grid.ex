2d308ded397f00176d98a07651a3c64c22adc063
Auralcat <miriamretka@gmail.com>
2020-11-04 20:30:57 -0300

    WIP - Create fetch_column and fetch_row functions
    These can be refactored even more, but it's pretty understandable as it
    is right now.

--- lib/grid.ex
+++ lib/grid.ex
@@ -6,28 +6,68 @@ defmodule SudokuValidator.Grid do
   defdelegate extract(raw_grid), to: SudokuValidator.GridExtractor, as: :extract
 
   @doc """
-  Fetches the row for a given sudoku grid structure.
+  Fetches the row with the given order number for a given sudoku grid structure.
   """
-  def fetch_row(grid, row_number) do
-    case row_number do
-      row_number when row_number in 0..2 ->
-        ["left", "center", "right"]
-        |> Enum.map(fn pos -> Enum.at(grid["upper"][pos], row_number) end)
-        |> Enum.reduce(fn cols, acc ->
-          Enum.concat(acc, cols)
-        end)
-      row_number when row_number in 3..5 ->
-        ["left", "center", "right"]
-        |> Enum.map(fn pos -> Enum.at(grid["center"][pos], row_number - 3) end)
-        |> Enum.reduce(fn cols, acc ->
-          Enum.concat(acc, cols)
-        end)
-      row_number when row_number in 6..8 ->
-        ["left", "center", "right"]
-        |> Enum.map(fn pos -> Enum.at(grid["lower"][pos], row_number - 6) end)
-        |> Enum.reduce(fn cols, acc ->
-          Enum.concat(acc, cols)
-        end)
-    end
+  def fetch_row(%{"upper" => upper_subgroups}, row_number) when row_number in 0..2 do
+    ["left", "center", "right"]
+    |> Enum.map(fn pos -> Enum.at(upper_subgroups[pos], row_number) end)
+    |> Enum.reduce(fn cols, acc ->
+      Enum.concat(acc, cols)
+    end)
+  end
+
+  def fetch_row(%{"center" => center_subgroups}, row_number) when row_number in 3..5 do
+    ["left", "center", "right"]
+    |> Enum.map(fn pos -> Enum.at(center_subgroups[pos], row_number - 3) end)
+    |> Enum.reduce(fn cols, acc ->
+      Enum.concat(acc, cols)
+    end)
+  end
+
+  def fetch_row(%{"lower" => lower_subgroups}, row_number) when row_number in 6..8 do
+    ["left", "center", "right"]
+    |> Enum.map(fn pos -> Enum.at(lower_subgroups[pos], row_number - 6) end)
+    |> Enum.reduce(fn cols, acc ->
+      Enum.concat(acc, cols)
+    end)
+  end
+
+  @doc """
+  Fetches the column for a given sudoku grid and the input column number.
+  """
+  def fetch_column(grid, col_number) when col_number in 0..2 do
+    ["upper", "center", "lower"]
+    |> Enum.map(fn section ->
+      Enum.map(0..2, fn pos ->
+        Enum.at(grid[section]["left"], pos) |> Enum.at(col_number)
+      end)
+    end)
+    |> Enum.reduce(fn cols, acc ->
+      Enum.concat(acc, cols)
+    end)
+  end
+
+  def fetch_column(grid, col_number) when col_number in 3..5 do
+    ["upper", "center", "lower"]
+    |> Enum.map(fn section ->
+      Enum.map(0..2, fn pos ->
+        Enum.at(grid[section]["center"], pos) |> Enum.at(col_number - 3)
+      end)
+    end)
+    |> Enum.reduce(fn cols, acc ->
+      Enum.concat(acc, cols)
+    end)
+  end
+
+  def fetch_column(grid, col_number) when col_number in 6..8 do
+    ["upper", "center", "lower"]
+    |> Enum.map(fn section ->
+      Enum.map(0..2, fn pos ->
+        Enum.at(grid[section]["right"], pos) |> Enum.at(col_number - 6)
+      end)
+    end)
+    |> Enum.reduce(fn cols, acc ->
+      Enum.concat(acc, cols)
+    end)
   end
 end
