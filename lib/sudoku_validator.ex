017927ff20d461c1bf11c9e447082c427faca51b
Auralcat <miriamretka@gmail.com>
2020-11-04 21:11:28 -0300

    Implement validation logic for sudoku sections
    With the grid extracted into a data structure and a way to fetch the
    parts we want from it, we can implement the validation logic and check
    if a given section is complete as well. Ultimately, this solves the problem.

--- lib/sudoku_validator.ex
+++ lib/sudoku_validator.ex
@@ -3,6 +3,10 @@ defmodule SudokuValidator do
   This module is responsible for returning if a Sudoku grid is valid.
   """
 
+  alias SudokuValidator.Grid
+
+  @incomplete_number_marker 0
+
   @doc """
   Returns a string saying if a sudoku grid is:
 
@@ -10,17 +14,72 @@ defmodule SudokuValidator do
   - Valid and incomplete
   - Invalid
   """
-  def validate(grid) do
-    {:ok, complete_valid_grid} = File.read(File.cwd! <> "/test/fixtures/valid_complete.sudoku")
-    {:ok, incomplete_valid_grid} = File.read(File.cwd! <> "/test/fixtures/valid_incomplete.sudoku")
+  def validate(raw_grid) do
+    parsed_grid = Grid.extract(raw_grid)
 
     cond do
-      grid == complete_valid_grid ->
-        "This sudoku grid is valid."
-      grid == incomplete_valid_grid ->
+      valid_grid?(parsed_grid) && incomplete_grid?(parsed_grid) ->
         "This sudoku grid is incomplete."
+
+      valid_grid?(parsed_grid) ->
+        "This sudoku grid is valid."
+
       true ->
         "This sudoku grid is invalid."
+    end
+  end
+
+  defp validate_row(grid, row_number) do
+    grid
+    |> Grid.fetch_row(row_number)
+    |> check_validity()
+  end
+
+  defp validate_column(grid, column_number) do
+    grid
+    |> Grid.fetch_column(column_number)
+    |> check_validity()
+  end
+
+  defp validate_subgroup(grid, subgroup_number) do
+    grid
+    |> Grid.fetch_subgroup(subgroup_number)
+    |> check_validity()
+  end
+
+  defp has_duplicate_numbers_in_list?(list) do
+    list
+    |> Enum.reject(fn number -> number == @incomplete_number_marker end)
+    |> Enum.any?(fn number ->
+      Enum.count(list, fn item -> item == number end) != 1
+    end)
+  end
+
+  defp valid_grid?(grid) do
+    rows = Enum.map(0..8, fn index -> validate_row(grid, index) end)
+    columns = Enum.map(0..8, fn index -> validate_column(grid, index) end)
+    subgroups = Enum.map(0..8, fn index -> validate_subgroup(grid, index) end)
+
+    [rows, columns, subgroups]
+    |> List.flatten()
+    |> Enum.all?(fn item -> item == :valid end)
+  end
+
+  defp incomplete_grid?(grid) do
+    rows = Enum.map(0..8, fn index -> Grid.fetch_row(grid, index) end)
+    columns = Enum.map(0..8, fn index -> Grid.fetch_column(grid, index) end)
+    subgroups = Enum.map(0..8, fn index -> Grid.fetch_subgroup(grid, index) end)
+
+    [rows, columns, subgroups]
+    |> List.flatten()
+    |> Enum.member?(@incomplete_number_marker)
+  end
+
+  defp check_validity(list) do
+    if has_duplicate_numbers_in_list?(list) do
+      :invalid
+    else
+      :valid
     end
   end
 end
